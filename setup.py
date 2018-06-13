46807ba1fe087d3b58c9da3d92cf79293fd59faa
wangsihong@live.com <wangsihong@live.com>
2018-06-13 14:46:33 +0800

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(  
     name = "deep-crf",
-    version = "1.5.1",
+    version = "1.5.2",
     keywords = ("lstm-crf", "crf"),
     description = "Sequence label tool base on Bi-LSTM-CRF mode.",
     long_description = "Sequence label tool base on Bi-LSTM-CRF mode.",
